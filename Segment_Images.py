2fb7880bfa079f90cbb5172fc7218fd590da6579
emdavis2 <elizabethmdav@gmail.com>
2023-04-20 16:25:50 -0400

    made a command line arguemnt for specifying path to folder with saved keras models and increased memory and time in slurm job submission scripts

--- Segment_Images.py
+++ Segment_Images.py
@@ -15,13 +15,17 @@ from tensorflow.keras import optimizers
 #path where preprocessed images are located to be segmented
 input_dir = str(sys.argv[1])
 
-#path where keras model is saved
-model_save_path = str(sys.argv[2])
+#path to model folder (keras_unet_models)
+path_to_model_dir = str(sys.argv[2])
+
+#name of saved keras model
+model_name = str(sys.argv[3])
 
 #path where to save segmented images 
-mask_type = str(sys.argv[3])
+mask_type = str(sys.argv[4])
 ####################################
 
+model_save_path = path_to_model_dir + '/' + model_name
 
 #call our trained model "loaded_model" and load it 
 loaded_model = keras.models.load_model(model_save_path)
--- nucleus_submitjob.sh
+++ nucleus_submitjob.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --time=3:00:00
-#SBATCH --mem=20g
+#SBATCH --time=10:00:00
+#SBATCH --mem=50g
 #SBATCH -p gpu
 #SBATCH --qos gpu_access
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=emae@med.unc.edu
+#SBATCH --mail-user=
 #SBATCH -o %j.out
 #SBATCH -e err.%j
 
 module add tensorflow_py3/2.1.0
-python ./Segment_Images.py '/proj/telston_lab/projects/data/2023_03_30_softgel_s1' '/nas/longleaf/home/emae/keras_unet_models/keras_unet_model_nucleus_segmenter_02152023_3trainingsets_varyingcontrasts_1024x1024imagesize' 'nucleus'
+python ./Segment_Images.py '/proj/telston_lab/projects/data/2023_03_30_softgel_s1' '/nas/longleaf/home/emae/Segmentation/keras_unet_models' 'keras_unet_model_nucleus_segmenter_02152023_3trainingsets_varyingcontrasts_1024x1024imagesize' 'nucleus'
