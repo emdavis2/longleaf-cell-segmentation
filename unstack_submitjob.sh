d5b49cb8aee89c4564e5e1ac3564d2f53dcdec94
emdavis2 <elizabethmdav@gmail.com>
2023-04-21 17:18:30 -0400

    job submission script for Unstack_TiffStack.py

---
+++ unstack_submitjob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=50g
+#SBATCH -p general
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=
+#SBATCH -o %j.out
+#SBATCH -e err.%j
+
+python ./Unstack_TiffStacks.py '/proj/telston_lab/projects/data/2023_04_19_Glass/DIC' '2023_04_19_Glass'
