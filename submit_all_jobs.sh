1dba8b9d6e0174a81602f30dc2c1ca274db7d9e0
emdavis2 <elizabethmdav@gmail.com>
2023-06-27 11:30:45 -0400

    increased wait time before submitting jobs

--- submit_all_jobs.sh
+++ submit_all_jobs.sh
@@ -5,7 +5,7 @@ IMG_PATH='/proj/telston_lab/projects/data/reformatted/2023_04_19_StiffGel'
 for i in {0..35}
 do
 sbatch membrane_submitjob.sh $i $IMG_PATH
-sleep 0.3
+sleep 1
 sbatch nucleus_submitjob.sh $i $IMG_PATH
-sleep 0.3
+sleep 1
 done
